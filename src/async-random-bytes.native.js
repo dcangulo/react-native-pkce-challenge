4b4d4dfac88739715018c0c538a6c051195fbed4
David Angulo <hello@davidangulo.xyz>
2022-02-01 10:23:13 +0800

    feat: expo SDK 40 support

--- src/async-random-bytes.native.js
+++ src/async-random-bytes.native.js
@@ -1,8 +1,20 @@
 const { NativeModules } = require('react-native');
+const { getRandomBytesAsync } = require('expo-random');
+const { Buffer } = require('buffer');
+const isExpo = require('./is-expo');
 
 function generateRandomBytes() {
   return new Promise((resolve) => {
-    NativeModules?.RandomBytes?.randomBytes(resolve);
+    if (isExpo) {
+      getRandomBytesAsync(96).then((u8) => {
+        const buffer = Buffer.from(u8);
+        const bytes = buffer.toString('base64');
+
+        resolve(bytes);
+      });
+    } else {
+      NativeModules?.RandomBytes?.randomBytes(resolve);
+    }
   });
 }
 
--- src/sync-random-bytes.native.js
+++ src/sync-random-bytes.native.js
@@ -1,6 +1,17 @@
 const CryptoJS = require('crypto-js');
+const { getRandomBytes } = require('expo-random');
+const { Buffer } = require('buffer');
+const isExpo = require('./is-expo');
 
 function generateRandomBytes() {
+  if (isExpo) {
+    const u8 = getRandomBytes(96);
+    const buffer = Buffer.from(u8);
+    const bytes = buffer.toString('base64');
+
+    return bytes;
+  }
+
   const buffer = CryptoJS.lib.WordArray.random(96);
   const bytes = buffer.toString(CryptoJS.enc.Base64);
 
