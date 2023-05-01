6f79fd3b6cbfbc4f01acb80c05ee41b67d11cff7
Konstantin <duell10111@t-online.de>
2023-05-01 15:07:56 +0200

    chore: update random bytes for new ExpoCrypto module

--- src/generate-random-bytes.native.ts
+++ src/generate-random-bytes.native.ts
@@ -4,19 +4,25 @@ import CryptoJS from 'crypto-js';
 import { BYTE_LENGTH } from './utils';
 
 export default function generateRandomBytes(): string {
+  if (NativeModules.ExpoCrypto) {
+    const bytes = NativeModules.ExpoCrypto.getRandomBase64String(BYTE_LENGTH);
+
+    return bytes;
+  }
+  
   if (NativeModules.ExpoRandom) {
     const bytes = NativeModules.ExpoRandom.getRandomBase64String(BYTE_LENGTH);
 
     return bytes;
   }
 
-  if ((global as any).ExpoModules) {
+  if ((global as any)?.ExpoModules?.ExpoRandom) {
     const bytes = (global as any).ExpoModules.ExpoRandom.getRandomBase64String(BYTE_LENGTH);
 
     return bytes;
   }
 
-  if ((global as any).RNPkceChallenge) {
+  if ((global as any)?.RNPkceChallenge) {
     const bytes = (global as any).RNPkceChallenge.randomBytes(BYTE_LENGTH);
 
     return bytes;
