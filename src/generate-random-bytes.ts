82e50cd3752d7199bc6dfa6cffcd74ed79229b50
David Angulo <hello@davidangulo.xyz>
2023-12-05 00:17:33 +0800

    feat: add node.js support

--- src/generate-random-bytes.ts
+++ src/generate-random-bytes.ts
@@ -1,8 +1,15 @@
 import { BYTE_LENGTH } from './utils';
 
 export default function generateRandomBytes(): string {
-  const buffer = window.crypto.getRandomValues(new Uint8Array(BYTE_LENGTH));
-  const bytes = btoa(String.fromCharCode(...new Uint8Array(buffer)));
+  if (typeof window !== 'undefined') {
+    const buffer = window.crypto.getRandomValues(new Uint8Array(BYTE_LENGTH));
+    const bytes = btoa(String.fromCharCode(...new Uint8Array(buffer)));
+
+    return bytes;
+  }
+
+  const buffer = require('crypto').randomBytes(BYTE_LENGTH);
+  const bytes = buffer.toString('base64');
 
   return bytes;
 }
