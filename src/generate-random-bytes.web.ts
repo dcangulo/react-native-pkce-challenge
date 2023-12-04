c71e65cbc95e742c847ec0cfe8b9d607354896e9
David Angulo <hello@davidangulo.xyz>
2023-12-05 01:15:45 +0800

    fix: build warning on web

--- src/generate-random-bytes.web.ts
+++ src/generate-random-bytes.web.ts
@@ -1,13 +1,6 @@
 import { BYTE_LENGTH } from './utils';
 
 export default function generateRandomBytes(): string {
-  if (typeof window === 'undefined') {
-    const buffer = require('crypto').randomBytes(BYTE_LENGTH);
-    const bytes = buffer.toString('base64');
-
-    return bytes;
-  }
-
   const buffer = window.crypto.getRandomValues(new Uint8Array(BYTE_LENGTH));
   const bytes = btoa(String.fromCharCode(...new Uint8Array(buffer)));
 
